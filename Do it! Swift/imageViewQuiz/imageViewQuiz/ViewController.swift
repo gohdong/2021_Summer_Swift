3ec4a035fb6d05351fddaa0479271c4959ba1e75
gohdong <aldehf420@naver.com>
2021-12-28 12:30:04 +0900

    quiz done

--- Do it! Swift/imageViewQuiz/imageViewQuiz/ViewController.swift
+++ Do it! Swift/imageViewQuiz/imageViewQuiz/ViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageIndex = 1
+    
+    @IBOutlet var imgView: UIImageView!
+    
+    @IBAction func btnImagePrevious(_ sender: UIButton) {
+        imageIndex -= 1
+        if imageIndex == 0{
+            imageIndex = 6
+        }
+        
+        imgView.image = UIImage(named: String(imageIndex)+".png")
+    }
+    @IBAction func btnImageNext(_ sender: UIButton) {
+        imageIndex += 1
+        if imageIndex == 7{
+            imageIndex = 1
+        }
+        
+        imgView.image = UIImage(named: String(imageIndex)+".png")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        imgView.image = UIImage(named: String(imageIndex)+".png")
         // Do any additional setup after loading the view.
     }
 
